84c2d66a779fd8ad44df55028f5f09124f8690e9
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2019-03-25 21:42:21 +0530

    Added scroll to top button

--- static/js/note.js
+++ static/js/note.js
@@ -2,11 +2,42 @@ const App = (function($){
 
     function init(){
         initTableOfContent();
+        
+        initFloatingButton();
+
+        initToolTip();
     }
 
     function initTableOfContent(){
 
         TOC($('#tableOfContent'));
+    }
+
+
+    function initFloatingButton(){
+        const target = $('.floating-icon-button');
+
+        // Show button when content is scrolled
+        $(window).scroll(function(){
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                target.show();
+            } else {
+                target.hide();
+            }
+        });
+
+        // On Click scroll to top
+        target.click(function(event){
+            $('html,body').animate({scrollTop: 0},'slow');
+            event.preventDefault();
+        });
+
+        // Hidden at start
+        target.hide();
+    }
+
+    function initToolTip(){
+        $('[data-toggle="tooltip"]').tooltip(); 
     }
 
     return {
