0fad463e4af2fbf587307efc3b718a1a88931f50
Sunit Deshpande <sunitdeshpande1234@gmail.com>
2019-03-27 23:59:15 +0530

    Added row layout to index page

--- static/js/index.js
+++ static/js/index.js
@@ -18,6 +18,8 @@ const App = (function ($) {
             
             itemSelector: '.book-card',
 
+            layoutMode: 'fitRows',
+
             filter: function () {
                 return searchRegex ? $(this).find('.book-title').text().match(searchRegex) : true;
             }
